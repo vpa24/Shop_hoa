ab2d9f39c36887ce01a42f639e57a0b7348c8295
vpa24 <vuphuonganh020497@gmail.com>
2018-01-09 18:04:46 +0700

    tăng số giây COOKIE khi đăng nhập thành công

--- admin/controllers/kiem_tra_session.php
+++ admin/controllers/kiem_tra_session.php
@@ -4,8 +4,8 @@ if (isset($_COOKIE['id'])==false) {
 }
 else{
   if(isset($_COOKIE['checked'])){
-    setcookie('checked', true, time() + 10);
-    setcookie('id', $_COOKIE['id'] , time() + 10);
+    setcookie('checked', true, time() + (60*2));
+    setcookie('id', $_COOKIE['id'] , time() + (60*2));
     setcookie('hoTen',$_COOKIE['hoTen'], time() + (60*2));
     setcookie('taiKhoan',$_COOKIE['taiKhoan'], time() + (60*2));
   }
