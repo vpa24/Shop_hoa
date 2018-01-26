e6ef57167b82f4410416576599c12d38759600bb
vpa24 <vuphuonganh020497@gmail.com>
2018-01-26 15:51:07 +0700

    sừa hiển thị admin

--- admin/smarty/templates_c/bf494c2e665ca8bf2491f3b134765a46dfce7db9_0.file.v_san_pham.tpl.php
+++ admin/smarty/templates_c/bf494c2e665ca8bf2491f3b134765a46dfce7db9_0.file.v_san_pham.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.30, created on 2018-01-18 07:57:16
+/* Smarty version 3.1.30, created on 2018-01-26 08:38:21
   from "C:\wamp64\www\shop_hoa-master\admin\views\index\v_san_pham.tpl" */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.30',
-  'unifunc' => 'content_5a60535ceca771_75950345',
+  'unifunc' => 'content_5a6ae8fd6c02c2_73961931',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'bf494c2e665ca8bf2491f3b134765a46dfce7db9' => 
     array (
       0 => 'C:\\wamp64\\www\\shop_hoa-master\\admin\\views\\index\\v_san_pham.tpl',
-      1 => 1516262235,
+      1 => 1516955846,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5a60535ceca771_75950345 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5a6ae8fd6c02c2_73961931 (Smarty_Internal_Template $_smarty_tpl) {
 ?>
 <div class="row">
   <div class="col-xl-3 col-sm-6 mb-3">
@@ -56,7 +56,22 @@ function content_5a60535ceca771_75950345 (Smarty_Internal_Template $_smarty_tpl)
       </div>
     </div>
   </div>
-
+  <div class="col-xl-3 col-sm-6 mb-3">
+    <div class="card text-white bg-info o-hidden h-100">
+      <div class="card-body">
+        <div class="card-body-icon">
+          <i class="fa fa-fw fa-file-o"></i>
+        </div>
+        <div class="mr-5">xem tất cả sản phẩm</div>
+      </div>
+      <a class="card-footer text-white clearfix small z-1" href="hoa.php">
+        <span class="float-left">Xem chi tiết</span>
+        <span class="float-right">
+          <i class="fa fa-angle-right"></i>
+        </span>
+      </a>
+    </div>
+  </div>
 
 </div>
 <?php }
--- admin/views/index/v_san_pham.tpl
+++ admin/views/index/v_san_pham.tpl
@@ -29,6 +29,21 @@
       </div>
     </div>
   </div>
-
+  <div class="col-xl-3 col-sm-6 mb-3">
+    <div class="card text-white bg-info o-hidden h-100">
+      <div class="card-body">
+        <div class="card-body-icon">
+          <i class="fa fa-fw fa-file-o"></i>
+        </div>
+        <div class="mr-5">xem tất cả sản phẩm</div>
+      </div>
+      <a class="card-footer text-white clearfix small z-1" href="hoa.php">
+        <span class="float-left">Xem chi tiết</span>
+        <span class="float-right">
+          <i class="fa fa-angle-right"></i>
+        </span>
+      </a>
+    </div>
+  </div>
 
 </div>
